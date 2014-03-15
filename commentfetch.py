c913204eb3342d46ab23f32a12cffef277031049
Balletie <skip_meesie@hotmail.com>
2014-03-16 00:11:11 +0100

    merged in some parts by /u/maula115

--- commentfetch.py
+++ commentfetch.py
@@ -3,9 +3,18 @@ import praw
 import textwrap
 import types
 from HTMLParser import HTMLParser
-	
+
+ts1 = '1310632807' # 7/4/2011
+ts2 = '1388023200' # 12/26/2013
+last_50 = deque(maxlen=50)
+user_name = 'michael_dorfman'
+subreddit_name = 'Buddhism'
+more_comments = praw.objects.MoreComments
 wrapper = textwrap.TextWrapper(width=100, break_long_words=False, replace_whitespace=False)
 h = HTMLParser()
+
+def query_str(ts1, ts2):
+	return 'timestamp:' + ts1 + '..' + ts2
 
 def print_comment_term(comment, level):
 	if isinstance(comment.author, types.NoneType):
@@ -52,20 +61,38 @@ def print_tree(commentTree, level, maxlevel=None):
 	print('</div>')
 
 r = praw.Reddit('commentfetcher by Skipperr')
-#submission = r.get_submission(submission_id='1xcy87')
-#comments = submission.comments
-#print_tree(comments, 0, 4)
+r.login(username='uname', password='pswd')
 
-redditor = r.get_redditor("michael_dorfman")
-m_comments = redditor.get_comments(limit=10000)
 print('<html><head>',
       '<link href=\"stylesheet.css\" rel=\"stylesheet\" type=\"text/css\"/>',
       '<script src=\"showhide.js\"></script>',
       '</head><body><a onclick=\"return hideAll()\" href=\"#\">Hide All</a><div>')
-for comment in m_comments:
-	if comment.parent_id == comment.link_id:
-		print('<div class=\"root\">')
-		print_comment(comment, 0)
-		print_tree(comment.replies, 1)
-		print('</div>')
+while True:
+	last_created = None
+	try: 
+		results_gen = r.search(query_str(ts1, ts2),
+				subreddit=subreddit_name,
+				sort='new',
+				syntax='cloudsearch', 
+				limit=None)
+	except HTTPError, e:
+		print 'Search from {0} to {1} produced error: {2}'.format(ts1, ts2, e.code)
+		sleep(60)
+		continue
+	for submission in results_gen:
+		if submission.id in last_50:
+			continue
+		last_50.append(submission.id)
+		last_created = str(trunc(submission.created_utc) + offset)
+		m_comments = submission.comments
+		for comment in m_comments:
+			if comment.parent_id == comment.link_id:
+				print('<div class=\"root\">')
+				print_comment(comment, 0)
+				print_tree(comment.replies, 1)
+				print('</div>')
+
+	if not last_created:
+		break
+	ts2 = last_created
 print('</div></body></html>')
